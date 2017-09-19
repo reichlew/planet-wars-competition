5ad2fb0d80c443eb2e36ce0d93913184497940ad
William Reichle <reichlew@gmail.com>
2017-09-19 11:04:34 -0400

    Update agent name to only allow 18 characters so that it looks good on the screen

--- PlanetWars/Validators/LogonRequestValidator.cs
+++ PlanetWars/Validators/LogonRequestValidator.cs
@@ -14,7 +14,7 @@ namespace PlanetWars.Validators
         {
             RuleFor(logon => logon.AgentName)
                 .NotEmpty().WithMessage("Please use a non empty agent name, logon failed")
-                .Length(1, 20).WithMessage("Please use an agent name between 1-20 characters, logon failed");
+                .Length(1, 18).WithMessage("Please use an agent name between 1-18 characters, logon failed");
 
             RuleFor(logon => logon.MapGeneration)
                 .Must(x => x == MapGenerationOption.Basic || x == MapGenerationOption.Random).WithMessage("Please use a valid Map Generation Option");
