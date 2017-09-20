832cdac0a04c8b98a8d3c74154fc3220ab4c5419
William Reichle <reichlew@gmail.com>
2017-09-20 08:04:36 -0400

    Move AgentBase to Shared

--- PlanetWars/Validators/LogonRequestValidator.cs
+++ PlanetWars/Validators/LogonRequestValidator.cs
@@ -16,8 +16,8 @@ namespace PlanetWars.Validators
                 .NotEmpty().WithMessage("Please use a non empty agent name, logon failed")
                 .Length(1, 18).WithMessage("Please use an agent name between 1-18 characters, logon failed");
 
-            RuleFor(logon => logon.MapGeneration)
-                .Must(x => x == MapGenerationOption.Basic || x == MapGenerationOption.Random).WithMessage("Please use a valid Map Generation Option");
+            RuleFor(logon => logon)
+                .Must(x => x.GameId > 0 || (x.MapGeneration == MapGenerationOption.Basic || x.MapGeneration == MapGenerationOption.Random)).WithMessage("Please use a valid Map Generation Option");
         }
     }
 }
